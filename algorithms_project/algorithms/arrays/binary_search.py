854875e2038d04e7d41f3558c666fb470b999c1d
marcosurbanski <marcos.urbanski@gmail.com>
2025-08-19 11:17:43 -0300

    implementado virtual env e alguns testes

--- algorithms_project/algorithms/arrays/binary_search.py
+++ algorithms_project/algorithms/arrays/binary_search.py
@@ -1,5 +1,5 @@
 class Binary:
-    def binary_search(nums, n):
+    def binary_search(nums,  n):
         low = 0
         high = len(nums)
         steps = 0
@@ -9,21 +9,22 @@ class Binary:
             mid = int((low + high)/2)
 
             if nums[mid] == n:
-                    print("step: ", steps)
-                    return mid
+                print("step: ",  steps)
+                return mid
             elif nums[mid] < n:
-                 low = mid+1
+                low = mid+1
             else:
-                 high = mid
+                high = mid
         return -1
-    
-a = [1,2,3,4,5]
-b = [1,2,3,4,5,6,7,8,9,10]
-c = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-d = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
+
+
+a = [1, 2, 3, 4, 5]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+d = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
 
 binary = Binary
-binary.binary_search(a, 3)
-binary.binary_search(b, 3)
-binary.binary_search(c, 3)
-binary.binary_search(d, 3)
+binary.binary_search(a,  3)
+binary.binary_search(b,  3)
+binary.binary_search(c,  3)
+binary.binary_search(d,  3)
