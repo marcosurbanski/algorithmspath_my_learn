60d315114f23e7a797b933cece4369b13411c6e8
marcosurbanski <marcos.urbanski@gmail.com>
2025-08-20 15:23:34 -0300

    implementado os testes para two_pointer

--- algorithms_project/tests/test_arrays/test_two_pointer.py
+++ algorithms_project/tests/test_arrays/test_two_pointer.py
@@ -1,5 +1,5 @@
 import pytest
-from algorithms.arrays.two_pointer import Solution
+from algorithms.arrays.two_pointer import Solution, Solution2, Solution3
 
 
 @pytest.mark.parametrize("input_str,expected", [
@@ -9,3 +9,16 @@ from algorithms.arrays.two_pointer import Solution
 ])
 def test_reverse_words(input_str, expected):
     assert Solution.reverseWords_manual(input_str) == expected
+
+
+@pytest.mark.parametrize("input_str,expected", [
+    ("I evol edocteel", "I love leetcode"),
+])
+def test_reverse_words1(input_str, expected):
+    assert Solution2.reverserWords(input_str) == expected
+
+@pytest.mark.parametrize("input_str,expected", [
+    ("Let's take LeetCode contest", "s'teL ekat edoCteeL tsetnoc"),
+])
+def test_reverse_words2(input_str, expected):
+    assert Solution3.reverseWords_manual1(input_str) == expected
