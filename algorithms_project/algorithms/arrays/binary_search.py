072ee92a55676633e9bbb15363503a890e5e158c
marcosurbanski <marcos.urbanski@gmail.com>
2025-08-21 09:39:54 -0300

    Some comments

--- algorithms_project/algorithms/arrays/binary_search.py
+++ algorithms_project/algorithms/arrays/binary_search.py
@@ -1,5 +1,16 @@
 class Binary:
+    # Define uma classe chamada Binary (apenas para organizar o método).
+    # Define a class called Binary (just to organize the method)
+
     def binary_search(nums,  target):
+        """
+        Define o método binary_search que recebe:
+            - nums: lista de números ordenados
+            - n: valor alvo (target) que queremos encontrar
+        Define the method binary_search which receives:
+            - nums: a sorted list
+            - target: The value to search for
+        """
         low = 0
         high = len(nums) - 1
         steps = 0
