ecd3f43685e86333eb1aae86edfe3ff39ec4b091
marcosurbanski <marcos.urbanski@gmail.com>
2025-08-26 09:49:33 -0300

    removido teste manual da classe exponential search

--- algorithms_project/algorithms/arrays/exponential_search.py
+++ algorithms_project/algorithms/arrays/exponential_search.py
@@ -26,11 +26,3 @@ class Solution():
             return i
 
         return Solution.binary_search(arr,  target,  i//2,  min(i, n-1))
-
-
-d = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
-target = 14
-solution = Solution()
-result = solution.exponencial_search(d,  target)
-
-print(f"Element found at index {result}")
